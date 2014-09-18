895ef9582356080f340aed74e3f94982cbe8f669
Max Clarke <maxwelljclarke@gmail.com>
2014-09-18 15:32:21 -0700

    Cleaned out unused classes

--- src/bumba/Node/RowNode.java
+++
@@ -1,8 +0,0 @@
-package bumba.Node;
-
-/**
- * Created by emcee on 9/6/13.
- */
-/*public class RowNode extends ContainerNode {
-
-}*/
--- src/bumba/map/Row.java
+++
@@ -1,5 +0,0 @@
-package bumba.map;
-
-/**
- * Created by emcee on 9/8/13.
- */
